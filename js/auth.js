7aadbc923b0d4c65dba839ee71477d1c4aa68b64
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-08-21 07:25:43 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,56 @@
 // Authentication BMTC
+const SUPABASE_URL =
+"https://yrvnmmascklkuzpjkwxn.supabase.co";
+
+
+const SUPABASE_KEY =
+"MASUKKAN_ANON_KEY";
+
+
+const client =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
+
+
+async function login(){
+
+
+const email =
+document.getElementById("email").value;
+
+
+const password =
+document.getElementById("password").value;
+
+
+
+const {data,error} =
+await client.auth.signInWithPassword({
+
+email,
+
+password
+
+});
+
+
+
+if(error){
+
+document.getElementById("message").innerHTML =
+"Login gagal";
+
+return;
+
+}
+
+
+
+window.location.href =
+"admin.html";
+
+
+}

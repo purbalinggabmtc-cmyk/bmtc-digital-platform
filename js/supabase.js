7a75e31a817f7039f84b2bd63e6f04a9a23e2cb1
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-08-21 07:23:57 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,10 @@
 // Supabase connection
+const SUPABASE_URL = "MASUKKAN_URL_SUPABASE";
+
+const SUPABASE_KEY = "MASUKKAN_ANON_KEY";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);

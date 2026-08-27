8ebf69268b749dcdf692a4d556e461bde5076f71
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-08-28 02:37:58 +0700

    Create navigation.js

---
+++ bold-after-dark/config/navigation.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+ * BOLD AFTER DARK
+ * CENTRAL NAVIGATION CONFIGURATION
+ *
+ * Semua halaman menggunakan file ini sebagai
+ * sumber alamat navigasi.
+ */
+
+window.BOLD_AFTER_DARK_NAVIGATION = Object.freeze({
+
+  home:
+    "https://boldafterdark.my.id/index.html",
+
+  admin:
+    "https://boldafterdark.my.id/admin.html",
+
+  report:
+    "https://boldafterdark.my.id/report.html",
+
+  scanner:
+    "https://boldafterdark.my.id/scanner.html",
+
+  registration:
+    "https://boldafterdark.my.id/registration/index.html"
+
+});
